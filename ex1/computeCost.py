8e883312f73706571ea4f141acce8c5b590c19ab
Kevin Casto <kcasto91@gmail.com>
2020-03-30 11:42:33 -0700

    Deleting extra files

--- ex1/computeCost.py
+++
@@ -1,4 +0,0 @@
-def computeCost(X, y, theta):
-    m = y.size
-    diff = X.dot(theta) - y
-    return diff.dot(diff)/(2*m)
--- ex1/normalEqn.py
+++
@@ -1,4 +0,0 @@
-import numpy as np
-
-def normalEqn(X, y):
-    return np.linalg.inv(X.T.dot(X)).dot(X.T.dot(y))
